df0fedc0066e72146d71c0f396883639c168ad2b
Sasho <sashoxrstov@abv.bg>
2016-06-27 19:07:15 +0300

    Classwork05

---
+++ src/Recursion/Main.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(sum(n));
+	}
+
+	private static int sum(int n) {
+		if (n == 1) {
+			return 1;
+		} else {
+			return n += sum(n - 1);
+		}
+	}
+}
